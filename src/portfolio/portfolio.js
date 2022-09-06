e7965d856a6946d1fece46413298951c014ec4f5
Camillarhi <rhitanene@gmail.com>
2022-09-06 13:13:13 +0100

    portfolio

---
+++ src/portfolio/portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../title/title'
+import Portfoliomenu from './portfoliomenu/portfoliomenu'
+
+export default function Portfolio() {
+  return (
+    <>
+    <Title title='Portfolio' />
+    <Portfoliomenu />
+    </>
+  )
+}

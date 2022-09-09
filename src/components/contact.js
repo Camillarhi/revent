287af860aaeecef7bff88d582af9e4805c17ae91
Camillarhi <rhitanene@gmail.com>
2022-09-09 05:42:04 +0100

    init

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,14 +4,11 @@ import CallLogo from '../image/call.png';
 import Line from '../image/line.png';
 import TopMail from '../image/topemail.png';
 import Form from './form';
-import Ring1 from '../image/ring1.png';
-import Ring2 from '../image/ring2.png';
-import Ring3 from '../image/ring3.png';
 import { Section } from './Section';
 
 export default function Contact() {
     return (
-        <main id='Contact' style={{ marginTop: '80px' }}>
+        <main  id='Contact' style={{ marginTop: '80px' }}>
             <div className='flx-direction-col aboutUsContainer isFlexed isCenteredCenter isCentered'>
                 <h1>Get in Touch</h1>
                 <img src={Line} alt='line' />
@@ -19,19 +16,16 @@ export default function Contact() {
             <Section style={{ gap: '20px', height: '100%' }}>
                 <section className='isFlexed isCenteredCenter contactSection isCentered'>
                     <div className='contact-image'>
-                        <div className='flx-direction-col isFlexed'>
-                            <img style={{ width: '13.79px', height: '13.79px' }} src={Ring1} alt={Ring1} />
-                            <img style={{ width: '9.56px', height: '9.56px' }} src={Ring2} alt={Ring2} />
-                            <img style={{ width: '5.52px', height: '5.52px' }} src={Ring3} alt={Ring3} />
-                            <img style={{ width: '40px', height: '40px' }} src={CallLogo} alt={CallLogo} />
+                        <div className='flx-direction-col isFlexed call'>
+                            <img className='callLogo' style={{ width: '40px', height: '40px' }} src={CallLogo} alt={CallLogo} />
                         </div>
                         <h3>Call Us</h3>
                         <p>+234-806 749-700</p>
                     </div>
                     <div className='contact-image'>
                         <div className='flx-direction-col isFlexed message'>
-                            <img src={TopMail} alt={TopMail} />
-                            <img src={BottomMail} alt={BottomMail} />
+                            <img className='topMail' src={TopMail} alt={TopMail} />
+                            <img className='bottomMail' src={BottomMail} alt={BottomMail} />
                         </div>
                         <h3>Send Email</h3>
                         <p>revent@digital.com</p>
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,37 +1,36 @@
 import React from 'react'
 import Line from '../image/line.png'
-import { Section } from './Section'
 
 export default function Resume() {
     return (
-        <main id='Experience' style={{marginTop:'80px'}}>
+        <main id='Experience' style={{ marginTop: '80px' }}>
             <div className='flx-direction-col aboutUsContainer isFlexed isCenteredCenter isCentered'>
                 <h1>Resume</h1>
                 <img src={Line} alt='line' />
-                <Section style={{ gap: '20px', height: '100%', width: '100%', padding: '0 80px' }}>
-                    <section className='isFlexed isCenteredCenter' style={{ flex: '2', margin: '0 auto' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around',width:'100%',marginTop:'10px' }}>
+                <section className='isFlexed' >
                         <div className='line'></div>
-                        <div>
+                        <div className='resumeSchool'>
                             <h4>2017</h4>
-                            <h5>Louisanna State University</h5>
+                            <h3>Louisanna State University</h3>
                             <p>Lorem Ipseum</p>
                             <h4 style={{ marginTop: '10px' }}>2018</h4>
-                            <h5>Havard EdX</h5>
+                            <h3>Havard EdX</h3>
                             <p>Principles of Design</p>
                         </div>
                     </section>
-                    <section className='isFlexed isCenteredCenter' style={{ margin: '0 auto' }}>
+                    <section className='isFlexed'>
                         <div className='line'></div>
-                        <div>
+                        <div className='resumeSchool'>
                             <h4>2019</h4>
-                            <h5>Udacity</h5>
+                            <h3>Udacity</h3>
                             <p >Product Design</p>
                             <h4 style={{ marginTop: '10px' }}>2020</h4>
-                            <h5>Microsoft Partner</h5>
+                            <h3>Microsoft Partner</h3>
                             <p >Platinum</p>
                         </div>
                     </section>
-                </Section>
+                </div>
             </div>
         </main>
     )
